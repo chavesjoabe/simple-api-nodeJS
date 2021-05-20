d686438b15af019e211318ed2edfc95155d5add6
chavesjoabe <chavesjoabe1@gmail.com>
2021-05-20 15:33:24 -0300

    final project

--- src/server.js
+++ src/server.js
@@ -1,30 +1,33 @@
-import express from 'express'
-import routes from './routes/routes'
-import mongoose from 'mongoose'
+import express from "express";
+import routes from "./routes/routes";
+import mongoose from "mongoose";
 
-const PORT = 3334
-const app = express()
+const PORT = 3334;
+const app = express();
 
-const DATA_BASE_URL = "mongodb+srv://joabe:1234@cluster0.hnm3u.mongodb.net/userTest?retryWrites=true&w=majority"
-const options = { poolSize: 5, useNewUrlParser: true, useUnifiedTopology: true }
+const DATA_BASE_URL = "<YOUR MONGO-DB DATABASE URL>";
+const options = {
+  poolSize: 5,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-mongoose.connect(DATA_BASE_URL, options)
+mongoose.connect(DATA_BASE_URL, options);
 
-const db = mongoose.connection
-db.on('error', () => {
-    console.log('error on database')
-})
-db.on('disconnected', () => {
-    console.log('database disconnected')
-})
-db.on('open', () => {
-    console.log('database connected')
-})
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("error on database");
+});
+db.on("disconnected", () => {
+  console.log("database disconnected");
+});
+db.on("open", () => {
+  console.log("database connected");
+});
 
-app.use(express.json())
-app.use(routes)
-
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-})
+  console.log(`server is running on port ${PORT}`);
+});
